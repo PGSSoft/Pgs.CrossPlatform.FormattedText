50b961192eb0a1b1088f190cc6f33a1ee74076a7
Maciej Kołodziej <mkolodziej@pgs-soft.com>
2016-10-28 09:10:05 +0200

    almost working(ned to unsort calls)

--- Pgs.CrossPlatform.FormattedText/Pgs.CrossPlatform.FormattedText.iOS/FormatConfig.cs
+++ Pgs.CrossPlatform.FormattedText/Pgs.CrossPlatform.FormattedText.iOS/FormatConfig.cs
@@ -1,7 +1,7 @@
 #if !_NuGetRelease_
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using CoreText;
 using Foundation;
 using Pgs.CrossPlatform.FormattedText.Core;
 using UIKit;
@@ -12,47 +12,58 @@ namespace Pgs.CrossPlatform.FormattedText.iOS
     {
         public static void Init(bool throwOnConfigLackchar, char tagStartChar = '<', char tagEndChar = '>')
         {
+            var sectionConfig = new Dictionary<string, object>()
+            {
+                {"b", true}, // for bold text true will be passed to IntersectedStyle
+                {"i", true},
+                {"backGreen", UIColor.Green},
+            };
+
             FormatParser.Instance.Initalize(new List<FormatTag>() {
                 new FormatTag("b", B), // first arg: tag name ex. <b>something</b>, where "b" is name
-                new FormatTag("i", I), // first arg: tag name ex. <b>something</b>, where "b" is name
-            }, throwOnConfigLackchar, tagStartChar, tagEndChar);
+                new FormatTag("i", I),
+                new FormatTag("backGreen", BackGreen),
+                new FormatTag("InterStyle", IntersectedStyle), 
+            }, throwOnConfigLackchar, tagStartChar, tagEndChar, sectionConfig);
+        }
+
+        private static void IntersectedStyle(object obj, object interConfig, int i1, int i2)
+        {
+            var configs = (Dictionary<string, object>) interConfig; // <UILabel, bool, bool, UIColor, UIColor>
+            object toSkipAllTheseOuts = new object();
+
+            var mutaStr = (NSMutableAttributedString) obj;
+            var control = configs["sourceControl"];
+
+
+            if (configs.TryGetValue("backGreen", out toSkipAllTheseOuts))
+                mutaStr.AddAttribute(UIStringAttributeKey.BackgroundColor, (UIColor)configs["backGreen"], new NSRange(i1, i2));
+
+            if (configs.TryGetValue("?", out toSkipAllTheseOuts))
+                mutaStr.AddAttribute(UIStringAttributeKey.ForegroundColor, (UIColor)configs["?"], new NSRange(i1, i2));
+
+            var traits = UIFontDescriptorSymbolicTraits.ClassUnknown;
+            if (configs.TryGetValue("b", out toSkipAllTheseOuts))
+                traits = UIFontDescriptorSymbolicTraits.Bold;
+            if (configs.TryGetValue("i", out toSkipAllTheseOuts))
+                traits = traits | UIFontDescriptorSymbolicTraits.Italic;
+            
+            var font = UIFont.FromDescriptor(((UILabel)control).Font.FontDescriptor.CreateWithTraits(traits), ((UILabel)control).Font.PointSize);
+            
+            mutaStr.AddAttribute(UIStringAttributeKey.Font, font, new NSRange(i1, i2));
+
+        }
+
+        public static void BackGreen(object obj, object sourceControl, int i1, int i2)
+        {
+            ((NSMutableAttributedString)obj).SetAttributes(new UIStringAttributes { BackgroundColor = UIColor.Green}, new NSRange(i1, i2));
         }
 
         public static void B(object obj, object sourceControl, int i1, int i2)
         {
             ApplyFontStyle(obj, sourceControl, i1, i2, UIFontDescriptorSymbolicTraits.Bold);
         }
-
-        private static void ApplyFontStyle(object obj, object sourceControl, int i1, int i2, UIFontDescriptorSymbolicTraits traitsToApply)
-        {
-            var mutableStr = ((NSMutableAttributedString) obj);
-
-            var existingRange = new NSRange(i1, i2 - i1);
-            var existingAttributes = mutableStr.GetAttributes(i1, out existingRange);
-
-            if (existingAttributes.Any() && existingRange.Equals(new NSRange(i1, i2 - i1)))
-            {
-                foreach (var attribute in existingAttributes)
-                {
-                    if (attribute.Key.Equals(UIStringAttributeKey.Font))
-                    {
-                        var value = (UIFont) attribute.Value;
-                        var traits = value.FontDescriptor.Traits;
-
-                        if (traits.SymbolicTrait != null)
-                            mutableStr.AddAttribute(UIStringAttributeKey.Font,
-                                CustomizeFont(sourceControl, traits.SymbolicTrait.Value | traitsToApply),
-                                new NSRange(i1, i2 - i1));
-                    }
-                }
-            }
-            else
-            {
-                mutableStr.AddAttribute(UIStringAttributeKey.Font,
-                    CustomizeFont(sourceControl, traitsToApply), new NSRange(i1, i2 - i1));
-            }
-        }
-
+        
         public static void I(object obj, object sourceControl, int i1, int i2)
         {
             ApplyFontStyle(obj, sourceControl, i1, i2, UIFontDescriptorSymbolicTraits.Italic);
@@ -65,6 +76,35 @@ namespace Pgs.CrossPlatform.FormattedText.iOS
             return font;
         }
 
+        private static void ApplyFontStyle(object obj, object sourceControl, int i1, int i2, UIFontDescriptorSymbolicTraits traitsToApply)
+        {
+            var mutableStr = ((NSMutableAttributedString)obj);
+
+            var existingRange = new NSRange(i1, i2);
+            var existingAttributes = mutableStr.GetAttributes(i1, out existingRange);
+
+            if (existingAttributes.Any() && existingRange.Equals(new NSRange(i1, i2)))
+            {
+                foreach (var attribute in existingAttributes)
+                {
+                    if (attribute.Key.Equals(UIStringAttributeKey.Font))
+                    {
+                        var value = (UIFont)attribute.Value;
+                        var traits = value.FontDescriptor.Traits;
+
+                        if (traits.SymbolicTrait != null)
+                            mutableStr.AddAttribute(UIStringAttributeKey.Font,
+                                CustomizeFont(sourceControl, traits.SymbolicTrait.Value | traitsToApply),
+                                new NSRange(i1, i2));
+                    }
+                }
+            }
+            else
+            {
+                mutableStr.AddAttribute(UIStringAttributeKey.Font,
+                    CustomizeFont(sourceControl, traitsToApply), new NSRange(i1, i2));
+            }
+        }
     }
 }
 #endif
